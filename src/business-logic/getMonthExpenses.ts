00acc716afd8ca5b11f79afa26ff6b1e9801882c
carloscrovara <carloscrovara@gmail.com>
2023-06-30 23:49:26 -0300

    Cambios menores

--- src/business-logic/getMonthExpenses.ts
+++ src/business-logic/getMonthExpenses.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../repository/prisma";
 
 //export async function getMonthExpenses(anio: string, mes: string, diaDesde: string, diaHasta: string){ 
-    export async function getMonthExpenses(anio: string, mes: string,){   
+    export async function getMonthExpenses(anio: string, mes: string){   
     try {
         const result = await prisma().$queryRaw`select * FROM gastos WHERE YEAR (fecha_creacion) = ${anio} AND MONTH (fecha_creacion) = ${mes}`;
         return result;
